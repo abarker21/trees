281eb1efa8437b47b7018bf91b92b1bde93b5ee4
alex <60376528+abarker21@users.noreply.github.com>
2020-04-17 23:22:41 -0700

    Update AVLTree.py

--- Trees/AVLTree.py
+++ Trees/AVLTree.py
@@ -69,7 +69,7 @@ class AVLTree(BST):
         The textbook's class hierarchy for their AVL tree code is fairly different from our class hierarchy,
         however, so you will have to adapt their code.
         '''
-        new_node. = Node(node.right.value)
+        new_node = Node(node.right.value)
         new_node.right = node.right.right
 
         new_left = Node(node.value)
